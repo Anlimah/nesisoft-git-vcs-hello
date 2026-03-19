da902501155f7d493014a08f373c30a0cc9e56a0
Anlimah <y.m.ratty7@gmail.com>
2026-03-19 20:03:25 +0000

    Program can display username entered

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    cout << "Hello Git!";
-    
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
     return 0;
 }
